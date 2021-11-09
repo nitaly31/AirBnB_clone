02834a9ae33727576f7a620c41126cfaa959548e
nitaly31 <geraldine.nitaly@gmail.com>
2021-11-08 17:37:59 -0800

    class BaseModel that defines all common attributes/methods. Task 3

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Class BaseModel """
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """
+    Clase base que define todos los
+    atributos / métodos comunes para otras clases
+    """
+    def __init__(self):
+        #Asigna id aleatorio
+        self.id = str(uuid.uuid4)
+        #Asigna fecha actual
+        self.created_at = datetime.now().isoformat()
+        #Actualiza la fecha de la ultima modificación
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """ devuelve el nombre de la clase, el ID y
+        el diccionario de atributos """
+        return ("[{}] ({}) {}"
+                .format(self.__class__.__name__, self.id, self.__dic__))
+
+    def save(self):
+        """ actualiza el atributo de instancia pública
+        updated_at con la fecha y hora actual """
+        self.updated_at = datatime.now().isoformat()
+
+    def to_dict(self):
+        """ devuelve un diccionario que contiene todas
+        las claves / valores de __dict__ de la instancia """
+        dictionary = self.__dict__
+        dictionary[self.__class__.__name__] = s
+        return dictionary
