39be131ff4611733a318357c22e6c6962b7fd21d
yuriquezada <quezadat.yuri@gmail.com>
2021-11-09 21:30:52 -0500

    Add methods do_EOF, emptyline and do_quit

--- console.py
+++ console.py
@@ -11,5 +11,17 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb) '
 
+    def do_EOF(self, line):
+        """End of File command: exit the program"""
+        return True
+
+    def emptyline(self):
+        """ Overwriting the emptyline method """
+        return False
+
+    def do_quit(self, line):
+        """Quit command exit the program"""
+        return True
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
