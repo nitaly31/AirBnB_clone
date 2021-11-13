d3c71feee155e5e05f071ef595a89075d04f6494
yuriquezada <quezadat.yuri@gmail.com>
2021-11-13 12:20:06 -0500

    Create do_destroy method

--- console.py
+++ console.py
@@ -64,6 +64,9 @@ class HBNBCommand(cmd.Cmd):
                 models.storage.save()
             else:
                 print("** no instance found **")
+    def do_destroy(self, line):
+        """Deletes an instance based on the class and id"""
+        className_line = line.split()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
