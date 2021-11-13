97b87455c26ca9c6e651bac24cd6891c36eb0b75
yuriquezada <quezadat.yuri@gmail.com>
2021-11-13 09:15:43 -0500

    Import classes from models

--- console.py
+++ console.py
@@ -4,6 +4,17 @@ Contains the entry point of the command interpreter
 """
 
 import cmd
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classGroup = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
+
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -41,5 +52,7 @@ class HBNBCommand(cmd.Cmd):
         if len(className_line) == 0:
             print("** class name missing **")
             return
+        # elif className_line[0] not in classGroup.keys():
+        #     print("** class doesn't exist **")
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
