3ac5e332082f399c057d1f7ac11855d28051c30d
yuriquezada <quezadat.yuri@gmail.com>
2021-11-20 08:50:59 -0500

    Add condition if instance id missing

--- console.py
+++ console.py
@@ -116,6 +116,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         elif className_line[0] not in classGroup.keys():
             print("** class doesn't exist **")
-
+        elif len(className_line) == 1:
+            print("** instance id missing **")
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
